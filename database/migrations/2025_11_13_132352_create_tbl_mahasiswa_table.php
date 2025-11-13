aff005e195ec919bbfc364b95e94526126744cb0
Lamgok <andosiahaan13@gmail.com>
2025-11-13 20:30:26 +0700

    push

---
+++ database/migrations/2025_11_13_132352_create_tbl_mahasiswa_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tbl_mahasiswa', function (Blueprint $table) {
+            // Kolom Primary Key sesuai model Mahasiswa.php
+            $table->string('nim')->primary(); 
+            
+            // Kolom data mahasiswa
+            $table->string('nama_mahasiswa');
+            $table->string('foto_url')->nullable();
+            $table->string('username')->unique();
+            $table->string('email_akademik')->unique();
+            $table->year('angkatan');
+            $table->date('tgl_masuk');
+            $table->string('program_studi');
+            $table->string('kelas');
+            $table->string('wali_mahasiswa');
+            $table->string('jalur_usm');
+            $table->string('status_akhir');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tbl_mahasiswa');
+    }
+};
